e58658b5c703d640e3abdc5cfcf2f7575fb18901
ConorClery <cleryc@tcd.ie>
2019-09-29 14:03:06 +0100

    Testing framework works now witha basic few tests and a quicksort algorithm

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,6 @@ module Main where
 
 import Lib
 
-main :: IO ()
-main = do
-  name <- getLine
-  ooft name
 
-ooft str = putStrLn str
+main :: IO ()
+main = someFunc  
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,19 @@
 import Test.Hspec
+import Test.Hspec.Expectations
 import Test.QuickCheck
 import Control.Exception (evaluate)
+import Test.Hspec.Core.Formatters
+import Quicksort
 
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "Quicksort_Basics" $ do
+        it "Test_Simple_List" $ do
+            let l = [10, 9, 8, 7, 6, 5]
+            let t1 = getCPUTime
+            quicksort l `shouldMatchList` [5, 6, 7, 8, 9, 10]
+
+        it "Test_Different_Simple_List" $ do
+            let l = [10, 9, 8, 7, 6, 5]
+            let t1 = getCPUTime
+            quicksort l `shouldMatchList` [5, 6, 7, 8, 9, 10]
